d5d44bf7f74adfbeb3056d6ee3bf83f16bf62da9
sumatetang <72253040+sumatetang@users.noreply.github.com>
2020-10-21 17:38:30 +0800

    Add files via upload

---
+++ Converter.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConverterApp.Models
+{
+    public class Converter
+    {/// <summary>
+     /// A less mathmatical approach to ASCII to Binary conversion
+     /// /*https://www.fluxbytes.com/csha*/rp/convert-string-to-binary-and-binary-to-string-in-c/
+     /// </summary>
+     /// <param name="text">String to convert</param>
+     /// <returns>Binary encoded string</returns>
+        public static string StringToBinary(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+       
+            foreach (char c in text.ToCharArray())
+            {
+                //Convert the char to base 2 and pad the output with 0
+                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+            }
+            return sb.ToString();
+        }
+
+
+        /// <summary>
+        /// Convert a Binary text string to a Text string
+        /// </summary>
+        /// <param name="text">Binary encoded string</param>
+        /// <returns>Text string</returns>
+        public static string BinaryToString(string text)
+        {
+            List<byte> bytes = new List<byte>();
+
+            for (int i = 0; i < text.Length; i += 8)
+            {
+                bytes.Add(Convert.ToByte(text.Substring(i, 8), 2));
+            }
+            return Encoding.ASCII.GetString(bytes.ToArray());
+        }
+
+
+
+
+     
+
+
+        /// <summary>
+        /// An approach to ASCII to Hexadecimal conversion using ToString("X2")
+        /// </summary>
+        /// <param name="data">String to convert</param>
+        /// <returns></returns>
+        public static string StringToHex2(string data)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            byte[] bytearray = Encoding.ASCII.GetBytes(data);
+
+            foreach (byte bytepart in bytearray)
+            {
+                sb.Append(bytepart.ToString("X2"));
+            }
+
+            return sb.ToString().ToUpper();
+        }
+
+
+        /// <summary>
+        /// Converts a Hexadecimal string to ASCII string
+        /// </summary>
+        /// <param name="hexString">Hexadecimal string</param>
+        /// <returns>ASCII string</returns>
+        public static string HexToString(string hexString)
+        {
+            if (hexString == null || (hexString.Length & 1) == 1)
+            {
+                throw new ArgumentException();
+            }
+            var sb = new StringBuilder();
+            for (var i = 0; i < hexString.Length; i += 2)
+            {
+                var hexChar = hexString.Substring(i, 2);
+                sb.Append((char)Convert.ToByte(hexChar, 16));
+            }
+            return sb.ToString();
+        }
+
+
+
+        /// <summary>
+        /// A more mathmatical approach to ASCII to Binary conversion
+        /// https://forums.asp.net/t/1713174.aspx?How+to+convert+ASCII+value+to+binary+value+using+c+net
+        /// </summary>
+        /// <param name="data">String to convert</param>
+        /// <returns>Binary encoded string</returns>
+        public static string StringToBinary2(string data)
+        {
+            string converted = string.Empty;
+            // convert string to byte array
+            byte[] bytes = Encoding.ASCII.GetBytes(data);
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    converted += (bytes[i] & 0x80) > 0 ? "1" : "0";
+                    bytes[i] <<= 1;
+                }
+            }
+
+            return converted;
+        
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using ConverterApp.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace ConverterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = Console.ReadLine();
+
+            var binary = Converter.StringToBinary(fullName);
+
+            Console.WriteLine(binary);
+
+
+            var ascii = Console.ReadLine();
+
+            string textFromBinary = Converter.BinaryToString(ascii);
+
+            Console.WriteLine($"Binary: {ascii}\nText: {textFromBinary}");
+
+
+            //convert string to byte array
+
+
+            byte[] fullNameBytes = Encoding.ASCII.GetBytes(fullName);
+
+            Console.WriteLine($"Text: {fullName}\nBytes: {fullNameBytes}");
+
+            foreach (byte b in fullNameBytes)
+            {
+                Console.WriteLine(b);
+            }
+
+
+
+            string unicodeString = fullName;
+            int[] cipher = new[] { 1, 1, 2, 3, 5, 8, 13 }; //Fibonacci Sequence
+            string cipherasString = String.Join(",", cipher.Select(x => x.ToString())); //FOr display
+
+            int encryptionDepth = 20;
+
+            Encrypter encrypter = new Encrypter(unicodeString, cipher, encryptionDepth);
+
+            //Single Level Encrytion
+            string nameEncryptWithCipher = Encrypter.EncryptWithCipher(unicodeString, cipher);
+            Console.WriteLine($"Encrypted once using the cipher {{{cipherasString}}} {nameEncryptWithCipher}");
+
+            string nameDecryptWithCipher = Encrypter.DecryptWithCipher(nameEncryptWithCipher, cipher);
+            Console.WriteLine($"Decrypted once using the cipher {{{cipherasString}}} {nameDecryptWithCipher}");
+
+            //Deep Encrytion
+            string nameDeepEncryptWithCipher = Encrypter.DeepEncryptWithCipher(unicodeString, cipher, encryptionDepth);
+            Console.WriteLine($"Deep Encrypted {encryptionDepth} times using the cipher {{{cipherasString}}} {nameDeepEncryptWithCipher}");
+
+            string nameDeepDecryptWithCipher = Encrypter.DeepDecryptWithCipher(nameDeepEncryptWithCipher, cipher, encryptionDepth);
+            Console.WriteLine($"Deep Decrypted {encryptionDepth} times using the cipher {{{cipherasString}}} {nameDeepDecryptWithCipher}");
+
+            //Base64 Encoded
+            Console.WriteLine($"Base64 encoded {unicodeString} {encrypter.Base64}");
+
+            string base64toPlainText = Encrypter.Base64ToString(encrypter.Base64);
+            Console.WriteLine($"Base64 decoded {encrypter.Base64} {base64toPlainText}");
+        }
+    }
+}
